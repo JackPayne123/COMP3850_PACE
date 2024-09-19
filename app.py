b8f87cb638f67114d46bb1def3e136416a750e49
JackPayne123 <jacktpayne51@gmail.com>
2024-09-19 09:09:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -427,11 +427,11 @@ if st.button("Run Verification"):
 
             st.markdown("### Verification Results")
             if authorship_result == "Authentic":
-                st.markdown(f"**Authorship Result:** {authorship_result}")
+                st.markdown(f"**Authorship Result:** {authorship_result} (Original Model: {model_choice})")
                 st.markdown(f"The predicted original model that generated the text is {model_choice}")
             else:
                 predicted_model = model_names[np.argmax(probabilities)]
-                st.markdown(f"**Authorship Result:** {authorship_result} ({predicted_model})")
+                st.markdown(f"**Authorship Result:** {authorship_result} ")
                 st.markdown(f"The predicted original model that generated the text is {predicted_model}")
             
             st.markdown("### Model Probabilities")
